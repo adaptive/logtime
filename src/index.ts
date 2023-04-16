4e4b55a9c84e1db64260f3fcb0d93f7df8802ab3
Hugo Romano <hugoromano@gmail.com>
2023-04-16 23:15:16 +0100

    revised

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,17 @@ import { Manager } from '@managed-components/types'
 export default async function (manager: Manager) {
   manager.addEventListener('pageview', async event => {
     const { client } = event
-    client.execute(`console.log(new Date().toString())`)
+
+    // Get the client's timestamp
+    const clientTime = client.timestamp
+
+    //format to Date
+    const date = new Date(clientTime).toString()
+
+    // Send the client's timestamp to the browser console
+    client.execute(`console.log("${date}")`)
+
+    // Alternatively, with a slight delay :)
+    //client.execute(`console.log(new Date().toString())`)
   })
 }
